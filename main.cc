2cc2414464e08e68e8627e487805dcc709da7954
Peter Munch <peterrmuench@gmail.com>
2021-08-28 12:26:31 +0200

    Refactor

--- main.cc
+++ main.cc
@@ -66,9 +66,6 @@ namespace Step26
     solve_time_step();
     void
     output_results() const;
-    void
-    refine_mesh(const unsigned int min_grid_level,
-                const unsigned int max_grid_level);
 
     Triangulation<dim> triangulation;
     FE_Q<dim>          fe;
@@ -79,17 +76,13 @@ namespace Step26
     SparsityPattern      sparsity_pattern;
     SparseMatrix<double> mass_matrix;
     SparseMatrix<double> laplace_matrix;
-    SparseMatrix<double> system_matrix;
 
     Vector<double> solution;
-    Vector<double> old_solution;
     Vector<double> system_rhs;
 
     double       time;
     double       time_step;
     unsigned int timestep_number;
-
-    const double theta;
   };
 
 
@@ -168,11 +161,102 @@ namespace Step26
 
 
   template <int dim>
+  class TimeIntegrationScheme
+  {
+  public:
+    TimeIntegrationScheme(const DoFHandler<dim> &          dof_handler,
+                          const AffineConstraints<double> &constraints,
+                          const SparseMatrix<double> &     mass_matrix,
+                          const SparseMatrix<double> &     laplace_matrix)
+      : theta(0.5)
+      , dof_handler(dof_handler)
+      , constraints(constraints)
+      , mass_matrix(mass_matrix)
+      , laplace_matrix(laplace_matrix)
+    {}
+
+    void
+    solve(Vector<double> &   solution,
+          const unsigned int timestep_number,
+          const double       time,
+          const double       time_step) const
+    {
+      std::cout << "Time step " << timestep_number << " at t=" << time
+                << std::endl;
+
+      SparseMatrix<double> system_matrix;
+      Vector<double>       system_rhs;
+      Vector<double>       tmp;
+      Vector<double>       forcing_terms;
+
+      system_matrix.reinit(mass_matrix.get_sparsity_pattern());
+      system_rhs.reinit(dof_handler.n_dofs());
+      tmp.reinit(dof_handler.n_dofs());
+      forcing_terms.reinit(dof_handler.n_dofs());
+
+      // create right-hand-side vector
+      mass_matrix.vmult(system_rhs, solution);
+
+      laplace_matrix.vmult(tmp, solution);
+      system_rhs.add(-(1 - theta) * time_step, tmp);
+
+      RightHandSide<dim> rhs_function;
+      rhs_function.set_time(time);
+      VectorTools::create_right_hand_side(dof_handler,
+                                          QGauss<dim>(
+                                            dof_handler.get_fe().degree + 1),
+                                          rhs_function,
+                                          tmp);
+      forcing_terms = tmp;
+      forcing_terms *= time_step * theta;
+
+      rhs_function.set_time(time - time_step);
+      VectorTools::create_right_hand_side(dof_handler,
+                                          QGauss<dim>(
+                                            dof_handler.get_fe().degree + 1),
+                                          rhs_function,
+                                          tmp,
+                                          constraints);
+
+      forcing_terms.add(time_step * (1 - theta), tmp);
+
+      system_rhs += forcing_terms;
+
+      // setup matrix
+      system_matrix.copy_from(mass_matrix);
+      system_matrix.add(theta * time_step, laplace_matrix);
+
+      SolverControl solver_control(1000, 1e-8 * system_rhs.l2_norm());
+      SolverCG<Vector<double>> cg(solver_control);
+
+      PreconditionSSOR<SparseMatrix<double>> preconditioner;
+      preconditioner.initialize(system_matrix, 1.0);
+
+      cg.solve(system_matrix, solution, system_rhs, preconditioner);
+
+      constraints.distribute(solution);
+
+      std::cout << "     " << solver_control.last_step() << " CG iterations."
+                << std::endl;
+    }
+
+  private:
+    const double theta;
+
+    const DoFHandler<dim> &          dof_handler;
+    const AffineConstraints<double> &constraints;
+
+    const SparseMatrix<double> &mass_matrix;
+    const SparseMatrix<double> &laplace_matrix;
+  };
+
+
+
+  template <int dim>
   HeatEquation<dim>::HeatEquation()
     : fe(1)
     , dof_handler(triangulation)
     , time_step(1. / 500)
-    , theta(0.5)
   {}
 
 
@@ -193,48 +277,30 @@ namespace Step26
 
     constraints.clear();
     DoFTools::make_hanging_node_constraints(dof_handler, constraints);
+    DoFTools::make_zero_boundary_constraints(dof_handler, 0, constraints);
     constraints.close();
 
     DynamicSparsityPattern dsp(dof_handler.n_dofs());
-    DoFTools::make_sparsity_pattern(dof_handler,
-                                    dsp,
-                                    constraints,
-                                    /*keep_constrained_dofs = */ true);
+    DoFTools::make_sparsity_pattern(dof_handler, dsp, constraints, true);
     sparsity_pattern.copy_from(dsp);
 
     mass_matrix.reinit(sparsity_pattern);
     laplace_matrix.reinit(sparsity_pattern);
-    system_matrix.reinit(sparsity_pattern);
 
-    MatrixCreator::create_mass_matrix(dof_handler,
-                                      QGauss<dim>(fe.degree + 1),
-                                      mass_matrix);
-    MatrixCreator::create_laplace_matrix(dof_handler,
-                                         QGauss<dim>(fe.degree + 1),
-                                         laplace_matrix);
+    MatrixCreator::create_mass_matrix<dim, dim, double>(dof_handler,
+                                                        QGauss<dim>(fe.degree +
+                                                                    1),
+                                                        mass_matrix,
+                                                        nullptr,
+                                                        constraints);
+    MatrixCreator::create_laplace_matrix<dim, dim>(dof_handler,
+                                                   QGauss<dim>(fe.degree + 1),
+                                                   laplace_matrix,
+                                                   nullptr,
+                                                   constraints);
 
     solution.reinit(dof_handler.n_dofs());
-    old_solution.reinit(dof_handler.n_dofs());
     system_rhs.reinit(dof_handler.n_dofs());
-  }
-
-
-  template <int dim>
-  void
-  HeatEquation<dim>::solve_time_step()
-  {
-    SolverControl            solver_control(1000, 1e-8 * system_rhs.l2_norm());
-    SolverCG<Vector<double>> cg(solver_control);
-
-    PreconditionSSOR<SparseMatrix<double>> preconditioner;
-    preconditioner.initialize(system_matrix, 1.0);
-
-    cg.solve(system_matrix, solution, system_rhs, preconditioner);
-
-    constraints.distribute(solution);
-
-    std::cout << "     " << solver_control.last_step() << " CG iterations."
-              << std::endl;
   }
 
 
@@ -259,164 +325,41 @@ namespace Step26
   }
 
 
-  template <int dim>
-  void
-  HeatEquation<dim>::refine_mesh(const unsigned int min_grid_level,
-                                 const unsigned int max_grid_level)
-  {
-    Vector<float> estimated_error_per_cell(triangulation.n_active_cells());
-
-    KellyErrorEstimator<dim>::estimate(
-      dof_handler,
-      QGauss<dim - 1>(fe.degree + 1),
-      std::map<types::boundary_id, const Function<dim> *>(),
-      solution,
-      estimated_error_per_cell);
-
-    GridRefinement::refine_and_coarsen_fixed_fraction(triangulation,
-                                                      estimated_error_per_cell,
-                                                      0.6,
-                                                      0.4);
-
-    if (triangulation.n_levels() > max_grid_level)
-      for (const auto &cell :
-           triangulation.active_cell_iterators_on_level(max_grid_level))
-        cell->clear_refine_flag();
-    for (const auto &cell :
-         triangulation.active_cell_iterators_on_level(min_grid_level))
-      cell->clear_coarsen_flag();
-
-    SolutionTransfer<dim> solution_trans(dof_handler);
-
-    Vector<double> previous_solution;
-    previous_solution = solution;
-    triangulation.prepare_coarsening_and_refinement();
-    solution_trans.prepare_for_coarsening_and_refinement(previous_solution);
-
-    triangulation.execute_coarsening_and_refinement();
-    setup_system();
-
-    solution_trans.interpolate(previous_solution, solution);
-    constraints.distribute(solution);
-  }
-
-
 
   template <int dim>
   void
   HeatEquation<dim>::run()
   {
-    const unsigned int initial_global_refinement       = 2;
-    const unsigned int n_adaptive_pre_refinement_steps = 4;
+    const unsigned int n_refinements = 4;
 
     GridGenerator::hyper_L(triangulation);
-    triangulation.refine_global(initial_global_refinement);
+    triangulation.refine_global(n_refinements);
 
     setup_system();
-
-    unsigned int pre_refinement_step = 0;
-
-    Vector<double> tmp;
-    Vector<double> forcing_terms;
-
-  start_time_iteration:
 
     time            = 0.0;
     timestep_number = 0;
 
-    tmp.reinit(solution.size());
-    forcing_terms.reinit(solution.size());
-
-
     VectorTools::interpolate(dof_handler,
                              Functions::ZeroFunction<dim>(),
-                             old_solution);
-    solution = old_solution;
+                             solution);
 
     output_results();
+
+    auto time_integration_scheme = std::make_unique<TimeIntegrationScheme<dim>>(
+      dof_handler, constraints, mass_matrix, laplace_matrix);
 
     while (time <= 0.5)
       {
         time += time_step;
         ++timestep_number;
 
-        std::cout << "Time step " << timestep_number << " at t=" << time
-                  << std::endl;
-
-        mass_matrix.vmult(system_rhs, old_solution);
-
-        laplace_matrix.vmult(tmp, old_solution);
-        system_rhs.add(-(1 - theta) * time_step, tmp);
-
-        RightHandSide<dim> rhs_function;
-        rhs_function.set_time(time);
-        VectorTools::create_right_hand_side(dof_handler,
-                                            QGauss<dim>(fe.degree + 1),
-                                            rhs_function,
-                                            tmp);
-        forcing_terms = tmp;
-        forcing_terms *= time_step * theta;
-
-        rhs_function.set_time(time - time_step);
-        VectorTools::create_right_hand_side(dof_handler,
-                                            QGauss<dim>(fe.degree + 1),
-                                            rhs_function,
-                                            tmp);
-
-        forcing_terms.add(time_step * (1 - theta), tmp);
-
-        system_rhs += forcing_terms;
-
-        system_matrix.copy_from(mass_matrix);
-        system_matrix.add(theta * time_step, laplace_matrix);
-
-        constraints.condense(system_matrix, system_rhs);
-
-        {
-          BoundaryValues<dim> boundary_values_function;
-          boundary_values_function.set_time(time);
-
-          std::map<types::global_dof_index, double> boundary_values;
-          VectorTools::interpolate_boundary_values(dof_handler,
-                                                   0,
-                                                   boundary_values_function,
-                                                   boundary_values);
-
-          MatrixTools::apply_boundary_values(boundary_values,
-                                             system_matrix,
-                                             solution,
-                                             system_rhs);
-        }
-
-        solve_time_step();
+        time_integration_scheme->solve(solution,
+                                       timestep_number,
+                                       time,
+                                       time_step);
 
         output_results();
-
-        if ((timestep_number == 1) &&
-            (pre_refinement_step < n_adaptive_pre_refinement_steps))
-          {
-            refine_mesh(initial_global_refinement,
-                        initial_global_refinement +
-                          n_adaptive_pre_refinement_steps);
-            ++pre_refinement_step;
-
-            tmp.reinit(solution.size());
-            forcing_terms.reinit(solution.size());
-
-            std::cout << std::endl;
-
-            goto start_time_iteration;
-          }
-        else if ((timestep_number > 0) && (timestep_number % 5 == 0))
-          {
-            refine_mesh(initial_global_refinement,
-                        initial_global_refinement +
-                          n_adaptive_pre_refinement_steps);
-            tmp.reinit(solution.size());
-            forcing_terms.reinit(solution.size());
-          }
-
-        old_solution = solution;
       }
   }
 } // namespace Step26
