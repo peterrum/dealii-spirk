0e08a3d3979661ab9ddc9479782c6a1ce2b89944
Peter Munch <peterrmuench@gmail.com>
2021-10-07 08:26:56 +0200

    Changes from SuperMUC

--- scripts/generate_json_files.py
+++ scripts/generate_json_files.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-def run_instance(scheme, degree, n_refinements):
+def run_instance(scheme, degree, n_refinements, n_stages):
     with open(os.path.dirname(os.path.abspath(__file__)) + "/generate_json_files.json", 'r') as f:
        datastore = json.load(f)
 
@@ -9,20 +9,37 @@ def run_instance(scheme, degree, n_refinements):
     datastore["FEDegree"]              = degree
     datastore["NRefinements"]          = n_refinements
     datastore["TimeIntegrationScheme"] = scheme
+    datastore["IRKStages"]             = n_stages
 
     # write data to output file
-    with open("./generate_json_files_%s_%s_%d.json" % (scheme, str(n_refinements).zfill(2), degree), 'w') as f:
+    with open("./generate_json_files_%s_%s_%d_%d.json" % (scheme, str(n_refinements).zfill(2), degree, n_stages), 'w') as f:
         json.dump(datastore, f, indent=4, separators=(',', ': '))
 
 def main():
     
     counter = 0;
 
-    for scheme in ["irk", "spirk"]:
-        for degree in [1,2]:
-            for n_refinements in range(2,20):
+    number_nodes     = 16
+    number_processes = 48 * number_nodes
 
-                run_instance(scheme, degree, n_refinements)
+    #for degree in [1,2]:
+    #    for n_stages in [2, 3, 9]:
+    for degree in [1]:
+        for n_stages in [3]:
+            for scheme in ["irk", "spirk"]:
+
+                print("array=($(ls generate_json_files_%s_*_%d_%d.json))" % (scheme, degree, n_stages))
+
+                if scheme == "irk":
+                    print("mpirun -np %d ./main \"${array[@]}\" | tee node-%s_%s-%d-%d.out" % (number_processes / n_stages, str(number_nodes).zfill(4), "irk0", degree, n_stages))
+                    print("mpirun -np %d ./main \"${array[@]}\" | tee node-%s_%s-%d-%d.out" % (number_processes, str(number_nodes).zfill(4), "irk1", degree, n_stages))
+                else:
+                    print("mpirun -np %d ./main \"${array[@]}\" | tee node-%s_%s-%d-%d.out" % (number_processes, str(number_nodes).zfill(4), "spirk", degree, n_stages))
+
+                print("")
+
+                for n_refinements in range(2,20):
+                    run_instance(scheme, degree, n_refinements, n_stages)
 
 
 if __name__== "__main__":
