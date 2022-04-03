711cabd5d56148ac58288324439f427fbd94b36a
Peter Munch <peterrmuench@gmail.com>
2022-04-03 16:15:37 +0200

    Test sm

--- scripts/parameters_vt.py
+++ scripts/parameters_vt.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-def run_instance(counter, n_refinements, scheme, do_row_major):
+def run_instance(counter, n_refinements, scheme, do_row_major, use_sm):
     with open(os.path.dirname(os.path.abspath(__file__)) + "/default.json", 'r') as f:
        datastore = json.load(f)
 
@@ -9,6 +9,11 @@ def run_instance(counter, n_refinements, scheme, do_row_major):
     datastore["NRefinements"]          = n_refinements
     datastore["TimeIntegrationScheme"] = scheme
     datastore["DoRowMajor"]            = do_row_major
+
+    if use_sm:
+        datastore["Padding"]         = 0
+        datastore["UseSharedMemory"] = True
+
 
     # write data to output file
     with open("./input_%s.json" % (str(counter).zfill(4)), 'w') as f:
@@ -19,15 +24,19 @@ def main():
 
     for n_refinements in range(3,20):
         # IRK
-        run_instance(counter, n_refinements, "irk", True)
+        run_instance(counter, n_refinements, "irk", True, False)
         counter = counter + 1;
 
         # SPIRK (row major)
-        run_instance(counter, n_refinements, "spirk", True)
+        run_instance(counter, n_refinements, "spirk", True, False)
+        counter = counter + 1;
+
+        # SPIRK (row major - sm)
+        run_instance(counter, n_refinements, "spirk", True, True)
         counter = counter + 1;
 
         # SPIRK (column major)
-        run_instance(counter, n_refinements, "spirk", False)
+        run_instance(counter, n_refinements, "spirk", False, False)
         counter = counter + 1;
 
 
